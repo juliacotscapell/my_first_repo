5c21a660bb4c753710162e3a0647d526d3bf7095
juliacotscapell <juliacots@gmail.com>
2022-09-15 15:50:54 +0200

    eliminate empty lines

--- my_first_repo.R
+++ my_first_repo.R
@@ -1,6 +1,4 @@
 # Set-up your script ------------------------------------------------------
-
-install.packages(c("tidyverse", "gapminder", "pacman")) # uncomment if already installed
 library(tidyverse)
 library(gapminder)
 library(pacman)
@@ -9,25 +7,21 @@ pacman::p_load(tidyverse, gapminder)
 
 
 # Load your Data into R ---------------------------------------------------
-
 data(gapminder)
 head(gapminder)
 
 
 # Clean your Data ---------------------------------------------------------
-
 gapminder_clean <- gapminder %>% 
   rename(life_exp = lifeExp, gdp_per_cap = gdpPercap) %>% 
   mutate(gdp = pop * gdp_per_cap)
 
 
 # Include only countries in the Americas -----------------------------------
-
 gapminder_Americas <- subset(gapminder_clean, continent=="Americas")
 
 
 # Rich / poor countries
-
 median(gapminder_Americas$gdp_per_cap) #to know the median value of all countries
 gapminder_Americas$economy <- ifelse(gapminder_Americas$gdp_per_cap>=5465.51, "rich", "poor")
 
